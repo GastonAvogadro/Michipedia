4c5ad4f7847680cf10723ac8095ac954a7ccea47
Gaston Avogadro <gaston.avogadro@gmail.com>
2023-06-16 00:35:14 -0300

    add favicon and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import Search from '@/components/Search';
+import michipedia from '@/assets/michipedia.svg'
 
 const Header = () => {
     return (
-        <header>
-            <h1>Michipedia</h1>
+        <header className='flex flex-col justify-center items-center p-10'>
+            <img src={michipedia} className='w-[300px]'/>
             <p>La enciclopedia que no sabías que necesitabas</p>
             <Search />
         </header>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { changeValue } from '@/redux/slices/searchSlice';
+import searchImg from '@/assets/search.svg';
 
 const Search = () => {
     const [word, setWord] = useState('');
     const dispatch = useDispatch();
 
-    const handleSearch = (search) => {
-        dispatch(changeValue(search));
+    const handleSearch = () => {
+        word !== '' && dispatch(changeValue(word));
     };
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
     return (
-        <>
-            <input type="text" placeholder="Buscar" onChange={(e) => setWord(e.target.value)} />
-            <button className="p-4 border border-black" onClick={() => handleSearch(word)}>
-                Buscar
+        <div className="flex justify-center items-center gap-2 mt-4">
+            <input
+                type="text"
+                placeholder="Buscar raza"
+                className="border border-black rounded-lg py-1 px-2 w-[250px]"
+                onChange={(e) => setWord(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <button className="border border-black" onClick={handleSearch}>
+                <img src={searchImg} className="w-[30px]" />
             </button>
-        </>
+        </div>
     );
 };
 
